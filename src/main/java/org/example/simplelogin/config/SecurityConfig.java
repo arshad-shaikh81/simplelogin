f915b8ce202179f8d66a509b1d82f06337a97320
arshad_shaikh81 <shekharshadulhak011@gmail.com>
2026-08-09 11:25:31 +0530

    changed url in security config file

--- src/main/java/org/example/simplelogin/config/SecurityConfig.java
+++ src/main/java/org/example/simplelogin/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:5500", "http://localhost:63342", "https://simplelogin-five.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:5500", "http://localhost:63342", "https://simplelogin-five.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
